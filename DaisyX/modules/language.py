c8a8a8e07197cfd017490a81cbfcc7f35109db08
InukaAsith <inukaasith7@gmail.com>
2022-03-12 15:00:36 +0000

    Auto Fixes: Code formatted

--- DaisyX/modules/language.py
+++ DaisyX/modules/language.py
@@ -76,11 +76,7 @@ async def select_lang_keyboard(message, strings, edit=False):
             )
         )
 
-    markup.add(
-        InlineKeyboardButton(
-            strings["crowdin_btn"], url="https://t.me/supbuz"
-        )
-    )
+    markup.add(InlineKeyboardButton(strings["crowdin_btn"], url="https://t.me/supbuz"))
     if edit:
         markup.add(InlineKeyboardButton(strings["back"], callback_data="go_to_start"))
     with suppress(MessageNotModified):
